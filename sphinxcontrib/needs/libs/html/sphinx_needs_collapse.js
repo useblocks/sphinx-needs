2fe1f29f9feef0e4512b7fca5b9a4c4b42ed104e
daniel <daniel.woste@useblocks.com>
2018-02-14 20:50:51 +0100

    Undo last bugfix

--- sphinxcontrib/needs/libs/html/sphinx_needs_collapse.js
+++ sphinxcontrib/needs/libs/html/sphinx_needs_collapse.js
@@ -1,5 +1,5 @@
-// $(document).ready(function() {
-$(window).on('load', function() {
+$(document).ready(function() {
+// $(window).on('load', function() {
     $(".toggle > *").hide();
     $(".toggle .header").show();
     $(".toggle .header").click(function() {
