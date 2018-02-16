8e54b38648d05f0c23df9cc286283b61b1ce275f
daniel <daniel.woste@useblocks.com>
2018-02-16 08:56:17 +0100

    fix for readthedocs table script

--- sphinxcontrib/needs/libs/html/sphinx_needs_collapse.js
+++ sphinxcontrib/needs/libs/html/sphinx_needs_collapse.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-// $(window).on('load', function() {
     $(".toggle > *").hide();
     $(".toggle .header").show();
     $(".toggle .header").click(function() {
@@ -13,7 +12,7 @@ $(document).ready(function() {
 // This display attribute does not work with the collapse function, so that we need to
 // correct this. Additionally we also need to set our needs-table on display:block, because it is no longer a
 // direct child of div.toggle and our script only manipulates direct children.
-$(window).on('load', function() {
+$(document).ready(function() {
     $(".toggle > .wy-table-responsive").hide();
     $(".wy-table-responsive > table").show();
 });
