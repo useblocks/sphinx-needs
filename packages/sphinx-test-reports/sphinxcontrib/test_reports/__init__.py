3c7a255507d08debf514b8adfcf882b7cc8211d4
daniel <daniel.woste@useblocks.com>
2018-04-26 10:24:45 +0200

    corrected setup.py setup and namespace handling

---
+++ packages/sphinx-test-reports/sphinxcontrib/test_reports/__init__.py
@@ -0,0 +1,1 @@
+from sphinxcontrib.test_reports.test_reports import setup  # NOQA
---
+++ packages/sphinx-test-reports/sphinxcontrib/test_reports/test_reports.py
@@ -0,0 +1,16 @@
+
+import sphinx
+from docutils import nodes
+from pkg_resources import parse_version
+
+sphinx_version = sphinx.__version__
+if parse_version(sphinx_version) >= parse_version("1.6"):
+    from sphinx.util import logging
+else:
+    import logging
+
+
+def setup(app):
+    log = logging.getLogger(__name__)
+
+    return {'version': '0.1.0'}  # identifies the version of our extension
