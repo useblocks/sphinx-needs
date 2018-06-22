99a43c541808c9331e0e581f63cd5ef08c2ccd2f
Trevor Lovett <trevor.lovett@gmail.com>
2018-06-22 09:53:31 -0500

    Fix failing test_styles tests.

--- tests/test_styles/test_style_unknown.py
+++ tests/test_styles/test_style_unknown.py
@@ -1,8 +1,12 @@
+from sphinx.builders.html import StandaloneHTMLBuilder
 from sphinx_testing import with_app
 
 
 @with_app(buildername='html', srcdir='doc_test/doc_style_unknown')
 def test_doc_style_unknown(app, status, warning):
+    # css_files is not cleared between test runs so css files get
+    # progressively added.  This forces it to clear before re-building
+    del StandaloneHTMLBuilder.css_files[:]
     app.build()
     html = (app.outdir / 'index.html').read_text()
     assert 'blank.css' in html
