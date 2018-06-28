72ffc0c16ffa43e807f87d34df0f11794f422e11
daniel <daniel.woste@useblocks.com>
2018-06-28 15:26:15 +0200

    raised version to 0.2.3

--- docs/conf.py
+++ docs/conf.py
@@ -39,9 +39,9 @@ sys.path.insert(0, os.path.abspath('../sphinxcontrib'))
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.2'
+version = '0.2.3'
 # The full version, including alpha/beta/rc tags.
-release = '0.2.2'
+release = '0.2.3'
 
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 if on_rtd:
