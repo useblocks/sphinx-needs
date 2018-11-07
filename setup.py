140c5e10c595ca2ca183495f3dff9e95f8835b08
Daniel Woste <daniel.woste@useblocks.com>
2018-11-07 10:22:36 +0100

    Added py37 support to setup.py

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Documentation',
         'Topic :: Utilities',
     ],
