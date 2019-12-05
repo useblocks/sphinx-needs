6907ceee81bf667971892845dadea9e2802ba780
Daniel Woste <daniel.woste@useblocks.com>
2019-12-05 08:41:59 +0100

    Raises version to 0.4.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = ['Sphinx<2;python_version<"3.5"',
 setup(
     name='sphinxcontrib-needs',
     # If you raise, think about versions in conf.py and needs.py!!!
-    version='0.4.2',
+    version='0.4.3',
     url='http://github.com/useblocks/sphinxcontrib-needs',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-needs',
     license='MIT',
