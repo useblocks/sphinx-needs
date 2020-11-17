a4a596113ddc593ed200d332cdd978d56688bffc
Daniel Woste <daniel.woste@useblocks.com>
2020-11-17 10:35:33 +0100

    Fix for role need_outgoing (#161)
    need_outgoing showed "none" in most situations.
    
    Fixes #160

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = ['Sphinx<2;python_version<"3.5"',
 setup(
     name='sphinxcontrib-needs',
     # If you raise, think about versions in conf.py and needs.py!!!
-    version='0.5.6',
+    version='0.5.7',
     url='http://github.com/useblocks/sphinxcontrib-needs',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-needs',
     license='MIT',
--- sphinxcontrib/needs/needs.py
+++ sphinxcontrib/needs/needs.py
@@ -43,7 +43,7 @@ else:
 
     logging.basicConfig()  # Only need to do this once
 
-VERSION = '0.5.6'
+VERSION = '0.5.7'
 
 NEEDS_FUNCTIONS_CONF = []
 
