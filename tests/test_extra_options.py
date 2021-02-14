7d725eccf0e0eb1e59f9b6360c601010a69e160e
Daniel Eades <danieleades@hotmail.com>
2021-02-14 10:16:14 +0000

    make token change to trigger CI

--- tests/test_extra_options.py
+++ tests/test_extra_options.py
@@ -4,11 +4,11 @@ from pathlib import Path
 from sphinx_testing import with_app
 
 
-@with_app(buildername='html', srcdir='doc_test/extra_options')
+@with_app(buildername="html", srcdir="doc_test/extra_options")
 def test_custom_attributes_appear(app, status, warning):
     app.build()
 
-    html = Path(app.outdir, 'index.html').read_text()
+    html = Path(app.outdir, "index.html").read_text()
 
     # Custom options should appear
     # assert 'introduced: <cite>1.0.0</cite>' in html
@@ -20,17 +20,17 @@ def test_custom_attributes_appear(app, status, warning):
     assert len(tables) == 4
 
     # First table should have all requirements
-    assert 'R_12345' in tables[2]
-    assert 'R_12346' in tables[2]
-    assert all(column in tables[2] for column in ('Introduced', 'Updated',
-                                                  'Impacts'))
+    assert "R_12345" in tables[2]
+    assert "R_12346" in tables[2]
+    assert all(column in tables[2] for column in ("Introduced", "Updated", "Impacts"))
 
     # Second table should only have component A requirements
-    assert 'R_12345' in tables[3]
-    assert 'R_12346' not in tables[3]
+    assert "R_12345" in tables[3]
+    assert "R_12346" not in tables[3]
 
     # Need list should only have component B requirements
-    items = re.findall('(<div class="line-block" id="needlist-index-.*?</div>)',
-                       html, re.DOTALL)
+    items = re.findall(
+        '(<div class="line-block" id="needlist-index-.*?</div>)', html, re.DOTALL
+    )
     assert len(items) == 1
-    assert 'R_12346' in items[0]
+    assert "R_12346" in items[0]
