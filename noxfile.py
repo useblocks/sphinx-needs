fef1df66ff5eff3cb5c5522552b130bedb1c2cac
Daniel Eades <danieleades@hotmail.com>
2021-03-01 13:33:18 +0000

    tidy noxfile

--- noxfile.py
+++ noxfile.py
@@ -4,26 +4,23 @@ from nox_poetry import session
 
 PYTHON_VERSIONS = ["3.6", "3.7", "3.8"]
 SPHINX_VERSIONS = ["2.2", "2.3", "2.4", "3.0", "3.2"]
+TEST_DEPENDENCIES = [
+    "nose",
+    "sphinx_testing",
+    "sphinxcontrib.plantuml",
+    "matplotlib",
+    "responses",
+    "sphinx_copybutton",
+]
 
 
 def is_supported(python: str, sphinx: str) -> bool:
-    if python == "3.6" and float(sphinx) > 3.0:
-        return False
-    return True
+    return not (python == "3.6" and float(sphinx) > 3.0)
 
 
 def run_tests(session, sphinx):
-    session.install(
-        *[
-            ".",
-            "nose",
-            "sphinx_testing",
-            "sphinxcontrib.plantuml",
-            "matplotlib",
-            "responses",
-            "sphinx_copybutton",
-        ]
-    )
+    session.install(".")
+    session.install(*TEST_DEPENDENCIES)
     session.run("pip", "install", f"sphinx=={sphinx}", silent=True)
     session.run("nosetests", "-w", "tests")
 
