cdd830c2b8551baab254c03f8b300b0c2ebb9805
Daniel Woste <daniel.woste@useblocks.com>
2021-04-20 10:17:13 +0200

    Checks github url for ending / (#237)
    Fixes #187

--- sphinxcontrib/needs/services/github.py
+++ sphinxcontrib/needs/services/github.py
@@ -27,6 +27,8 @@ class GithubService(BaseService):
         self.config = config
 
         self.url = self.config.get("url", "https://api.github.com/")
+        if not self.url.endswith("/"):
+            self.url = f"{self.url}/"
         self.max_amount = self.config.get("max_amount", -1)
         self.max_content_lines = self.config.get("max_content_lines", -1)
         self.id_prefix = self.config.get("id_prefix", "GITHUB_")
