f68b99e740e5ce632b3df4507c8ab36e3869173c
Daniel Woste <daniel.woste@useblocks.com>
2021-07-06 16:02:15 +0200

    Release 0.7.0

--- docs/conf.py
+++ docs/conf.py
@@ -41,9 +41,9 @@ sys.path.insert(0, os.path.abspath('../sphinxcontrib'))
 # built documents.
 #
 # The short X.Y version.
-version = '0.6'
+version = '0.7'
 # The full version, including alpha/beta/rc tags.
-release = '0.6.3'
+release = '0.7.0'
 
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 
