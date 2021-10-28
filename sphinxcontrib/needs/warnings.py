13adf40dfe814e5fa8a74b9af9479b9716053190
Haiyang <haiyang.zhang@tutanota.com>
2021-10-28 10:43:29 +0200

    Fixed sphinx-needs warnings return code (#396)
    * Fixed sphinx-needs warnings return code
    
    * Added check condition and more test scenarios
    
    Fix for #344

--- sphinxcontrib/needs/warnings.py
+++ sphinxcontrib/needs/warnings.py
@@ -72,6 +72,16 @@ def process_warnings(app, exception):
             else:
                 need_ids = [x["id"] for x in result]
 
+                # Set Sphinx statuscode to 1, only if -W is used with sphinx-build
+                # Because Sphinx statuscode got calculated in very early build phase and based on warning_count
+                # Sphinx-needs warnings check hasn't happened yet
+                # see deatils in https://github.com/sphinx-doc/sphinx/blob/81a4fd973d4cfcb25d01a7b0be62cdb28f82406d/sphinx/application.py#L345 # noqa
+                # To be clear, app.keep_going = -W and --keep-going, and will overrite -W after
+                # see details in https://github.com/sphinx-doc/sphinx/blob/4.x/sphinx/application.py#L182
+                if app.statuscode == 0:
+                    if app.keep_going or app.warningiserror:
+                        app.statuscode = 1
+
                 # get the text for used filter, either from filter string or function name
                 if hasattr(warning_filter, "__call__"):
                     warning_text = getattr(warning_filter, "__name__")
--- tests/test_needs_warning.py
+++ tests/test_needs_warning.py
@@ -29,3 +29,50 @@ def test_needs_warnings(app, status, warning):
 
     # Check warnings not including external needs
     assert "EXT_TEST_01" not in warnings
+
+
+@with_app(buildername="html", srcdir="doc_test/doc_needs_warnings_return_status_code")
+def test_needs_warnings_return_status_code(app, status, warning):
+    import os
+    import subprocess
+
+    srcdir = "doc_test/doc_needs_warnings_return_status_code"
+    out_dir = os.path.join(srcdir, "_build")
+
+    # Check return code when "-W --keep-going" not used
+    out_normal = subprocess.run(["sphinx-build", "-M", "html", srcdir, out_dir], capture_output=True)
+    assert out_normal.returncode == 0
+
+    # Check return code when only "-W" is used
+    out_w = subprocess.run(["sphinx-build", "-M", "html", srcdir, out_dir, "-W"], capture_output=True)
+    assert out_w.returncode >= 1
+
+    # Check return code when only "--keep-going" is used
+    out_keep_going = subprocess.run(
+        ["sphinx-build", "-M", "html", srcdir, out_dir, "--keep-going"], capture_output=True
+    )
+    assert out_keep_going.returncode == 0
+
+    # Check return code when "-W --keep-going" is used
+    out_w_keep_going = subprocess.run(
+        ["sphinx-build", "-M", "html", srcdir, out_dir, "-W", "--keep-going"], capture_output=True
+    )
+    assert out_w_keep_going.returncode == 1
+
+    # Check no Sphinx raised warnings
+    assert "WARNING" not in out_w_keep_going.stdout.decode("utf-8")
+
+    warnings = out_w_keep_going.stderr.decode("utf-8")
+
+    # Check Sphinx-needs raised warnings amount
+    assert warnings.count("WARNING: ") == 2
+
+    # Check warnings contents
+    assert "WARNING: invalid_status: failed" in warnings
+    assert "failed needs: 2 (SP_TOO_001, US_63252)" in warnings
+    assert "used filter: status not in ['open', 'closed', 'done', 'example_2', 'example_3']" in warnings
+
+    # Check needs warning from custom defined filter code
+    assert "WARNING: type_match: failed" in warnings
+    assert "failed needs: 1 (TC_001)" in warnings
+    assert "used filter: my_custom_warning_check" in warnings
