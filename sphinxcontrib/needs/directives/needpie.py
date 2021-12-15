e2cc28f1243fa8e18f67950f7bd3c287432edaf4
PhilipPartsch <philip.partsch@googlemail.com>
2021-12-16 00:49:08 +0100

    fixed format issue

--- sphinxcontrib/needs/directives/needpie.py
+++ sphinxcontrib/needs/directives/needpie.py
@@ -209,7 +209,6 @@ def process_needpie(app, doctree, fromdocname):
 
         #close the figure, to free consumed memory. 
         #Otherwise we will get: RuntimeWarning from matplotlib:
-        #More than 20 figures have been opened. Figures created through the pyplot interface (`matplotlib.pyplot.figure`) are retained until explicitly closed and may consume too much memory. (To control this warning, see the rcParam `figure.max_open_warning`)
         matplotlib.pyplot.close(fig) 
 
 def label_calc(pct, allvals):
