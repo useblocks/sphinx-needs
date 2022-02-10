57ea142883ede21afd3a74a5d32bc6d59b0107e3
PhilipPartsch <philip.partsch@googlemail.com>
2022-02-10 22:08:54 +0100

    fixed nested needs test

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -45,8 +45,7 @@ def test_filter_build_html(app, status, warning):
     assert "CHILD_2_STORY" in html_5
     assert (
         '<div class="line">child needs: <span class="parent_needs"><span><a class="reference internal" '
-        'href="#CHILD_1_STORY" title="STORY_PARENT">CHILD_1_STORY</a>, <a class="reference internal" '
-        'href="#CHILD_2_STORY" title="STORY_PARENT">CHILD_2_STORY</a></span></span></div>' in html_5
+        'href="#CHILD_1_STORY" title="STORY_PARENT">CHILD_1_STORY</a></span></span></div>' in html_5
     )
     assert (
         '<div class="line">parent needs: <span class="parent_needs"><span><a class="reference internal" '
