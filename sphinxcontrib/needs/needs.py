9119811f3712d0167096daeaf07be269eb9941f6
Daniel Woste <daniel.woste@useblocks.com>
2022-02-28 08:42:08 +0100

    Version raise to 0.7.6

--- sphinxcontrib/needs/needs.py
+++ sphinxcontrib/needs/needs.py
@@ -98,7 +98,7 @@ from sphinxcontrib.needs.services.manager import ServiceManager
 from sphinxcontrib.needs.utils import INTERNALS, NEEDS_FUNCTIONS
 from sphinxcontrib.needs.warnings import process_warnings
 
-VERSION = "0.7.5"
+VERSION = "0.7.6"
 NEEDS_FUNCTIONS.clear()
 
 
