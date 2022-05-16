5401bc0a59b11f14379c4e69cb1789ae504f95c5
Daniel Eades <danieleades@hotmail.com>
2022-05-17 07:48:44 +1000

    remove sphinx <= 1.6 compat

--- sphinxcontrib/needs/logging.py
+++ sphinxcontrib/needs/logging.py
@@ -1,13 +1,4 @@
-import sphinx
-from pkg_resources import parse_version
-
-sphinx_version = sphinx.__version__
-if parse_version(sphinx_version) >= parse_version("1.6"):
-    from sphinx.util import logging
-else:
-    import logging
-
-    logging.basicConfig()  # Only need to do this once
+from sphinx.util import logging
 
 
 def get_logger(name):
