20412c3c7e900b9bbf53bf43de19c1c13d5d597a
Randy Duodu (He/Him) <duodurandy19@gmail.com>
2022-05-18 15:42:09 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -422,8 +422,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = os.getenv("NEEDS_THEME", "sphinx_immaterial")
-html_theme = os.getenv("NEEDS_THEME", "alabaster")
+html_theme = os.getenv("NEEDS_THEME", "sphinx_immaterial")
+# html_theme = os.getenv("NEEDS_THEME", "alabaster")
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
