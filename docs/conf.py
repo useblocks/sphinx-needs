6620da5803e4cb3c374dca28ab031b03fe148ca6
Randy Duodu (He/Him) <duodurandy19@gmail.com>
2022-05-18 15:42:09 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -401,7 +401,6 @@ todo_include_todos = False
 #
 html_theme = os.getenv("NEEDS_THEME", "sphinx_immaterial")
 
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
