1b9819b62ac48967fa60380483b7a7faf2a7e116
Daniel Woste <daniel.woste@useblocks.com>
2022-05-19 21:35:16 +0200

    CI runner fixes

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ import nox
 from nox_poetry import session
 
 PYTHON_VERSIONS = ["3.8", "3.9.7", "3.10"]
-SPHINX_VERSIONS = ["4.0.3", "4.3.2", "4.5.0"]
+SPHINX_VERSIONS = ["4.0.3", "4.3.2", "4.5"]
 TEST_DEPENDENCIES = [
     "pytest",
     "pytest-xdist",
