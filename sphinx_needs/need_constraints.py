6f6580986af10b8360ed3d04358384e7e34d34f6
Martin <martin.a.wieland@gmx.de>
2022-06-13 23:59:19 +0200

    comments and refactoring pt I

--- sphinx_needs/need_constraints.py
+++ sphinx_needs/need_constraints.py
@@ -9,10 +9,8 @@ def process_constraints(app, need):
     """
     Finally creates the need-node in the docurils node-tree.
 
-    :param app:
-    :param doctree:
-    :param fromdocname:
-    :return:
+    :param app: sphinx app for access to config files
+    :param need: need object to process
     """
 
     config_constraints = app.config.needs_constraints
@@ -23,6 +21,7 @@ def process_constraints(app, need):
 
     for constraint in constraints:
 
+        # check if constraint is defined in config
         if constraint not in [x for x in config_constraints.keys()]:
             raise NeedsConstraintNotAllowed(
                 f"Constraint {constraint} of need id {need_id} is not allowed by config value 'needs_constraints'."
@@ -32,7 +31,10 @@ def process_constraints(app, need):
             # access constraints defined in conf.py
             executable_constraints = config_constraints[constraint]
 
+            # lazily gather all results to determine results_passed later
             results_list = []
+
+            # name is check_0, check_1, ...
             for name, cmd in executable_constraints.items():
                 # compile constraint and check single need if it fulfills constraint
                 if name != "severity":
@@ -42,13 +44,14 @@ def process_constraints(app, need):
                     results_list.append(constraint_passed)
 
                     if not constraint_passed:
-                        # prepare structure
+                        # prepare structure per name
                         if constraint not in need["constraints_results"].keys():
                             need["constraints_results"][constraint] = {}
 
                         # defines what to do if a constraint is not fulfilled. from conf.py
                         constraint_failed_options = app.config.needs_constraint_failed_options
 
+                        # prepare structure for check_0, check_1 ...
                         if name not in need["constraints_results"][constraint].keys():
                             need["constraints_results"][constraint][name] = {}
 
@@ -57,7 +60,7 @@ def process_constraints(app, need):
                         # severity of failed constraint
                         severity = executable_constraints["severity"]
 
-                        # configureable force of constraint failed style
+                        # configurable force of constraint failed style
                         force_style = constraint_failed_options[severity]["force_style"]
 
                         actions_on_fail = constraint_failed_options[severity]["on_fail"]
@@ -77,6 +80,7 @@ def process_constraints(app, need):
 
                         old_style = need["style"]
 
+                        # append to style if present
                         if old_style and len(old_style) > 0:
                             new_styles = "".join(", " + x for x in style_on_fail)
                         else:
@@ -89,10 +93,10 @@ def process_constraints(app, need):
                             constraint_failed_style = old_style + new_styles
                             need["style"] = constraint_failed_style
 
-
                     else:
-
                         # constraint is met, fill corresponding need attributes
+
+                        # prepare structure
                         if constraint not in need["constraints_results"].keys():
                             need["constraints_results"][constraint] = {}
                         need["constraints_results"][constraint][name] = constraint_passed
@@ -103,4 +107,3 @@ def process_constraints(app, need):
             else:
                 need["constraints_passed"] = True
 
-    return need
