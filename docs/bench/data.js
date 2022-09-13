55ded7e6b79fe6ffcf8358a610a250686ef3bf6c
github-action-benchmark <github@users.noreply.github.com>
2022-09-13 12:47:03 +0000

    add Benchmark (pytest) benchmark result for 0d04718230143255d48d6ceb2c73c84230eeb829

---
+++ docs/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1663073223682,
+  "repoUrl": "https://github.com/useblocks/sphinx-needs",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "useblocks",
+            "username": "useblocks"
+          },
+          "committer": {
+            "name": "useblocks",
+            "username": "useblocks"
+          },
+          "id": "0d04718230143255d48d6ceb2c73c84230eeb829",
+          "message": "Adds test benchmarks",
+          "timestamp": "2022-08-11T16:41:53Z",
+          "url": "https://github.com/useblocks/sphinx-needs/pull/695/commits/0d04718230143255d48d6ceb2c73c84230eeb829"
+        },
+        "date": 1663073219003,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmark/test_basic.py::test_benchmark_build_html[test_app0]",
+            "value": 2.5086521153672905,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 398.6204359999874 msec\nrounds: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
