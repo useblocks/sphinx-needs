19784bb3c728f5e6e0caf9a77a11d1b5459fdcf6
github-action-benchmark <github@users.noreply.github.com>
2022-09-14 09:35:41 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for a9b038b6ccaabad8c57fe3ec0098c2e076a939d9

---
+++ docs/bench/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1663148141053,
+  "repoUrl": "https://github.com/useblocks/sphinx-needs",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "useblocks",
+            "username": "useblocks"
+          },
+          "committer": {
+            "name": "useblocks",
+            "username": "useblocks"
+          },
+          "id": "a9b038b6ccaabad8c57fe3ec0098c2e076a939d9",
+          "message": "Adds test benchmarks",
+          "timestamp": "2022-08-11T16:41:53Z",
+          "url": "https://github.com/useblocks/sphinx-needs/pull/695/commits/a9b038b6ccaabad8c57fe3ec0098c2e076a939d9"
+        },
+        "date": 1663148136535,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "test_basic",
+            "value": 0.37779026799995563,
+            "unit": "s",
+            "extra": "Commit: 997a0d7f66b0f8cb72887e0593e14e60093cae91\nBranch: (detached head)\nTime: 2022-09-14T09:30:13+00:00"
+          },
+          {
+            "name": "test_official",
+            "value": 87.472825479,
+            "unit": "s",
+            "extra": "Commit: 997a0d7f66b0f8cb72887e0593e14e60093cae91\nBranch: (detached head)\nTime: 2022-09-14T09:30:13+00:00"
+          }
+        ]
+      }
+    ]
+  }
+}
