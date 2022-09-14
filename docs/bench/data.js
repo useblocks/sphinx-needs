7c15ca3625b0a268ea05f805c2d57e8c74114e07
github-action-benchmark <github@users.noreply.github.com>
2022-09-14 11:05:08 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 48d46667e868140524da763b9bde3fa18f0a08cf

--- docs/bench/data.js
+++ docs/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663148720516,
+  "lastUpdate": 1663153508295,
   "repoUrl": "https://github.com/useblocks/sphinx-needs",
   "entries": {
     "Benchmark": [
@@ -32,6 +32,42 @@ window.BENCHMARK_DATA = {
             "value": 66.33659909299999,
             "unit": "s",
             "extra": "Commit: a397e7ed0a02c6620d5bf36053ac256549960e37\nBranch: (detached head)\nTime: 2022-09-14T09:40:56+00:00"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "daniel.woste@useblocks.com",
+            "name": "Daniel Woste",
+            "username": "danwos"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "48d46667e868140524da763b9bde3fa18f0a08cf",
+          "message": "Benchmarks only on master-commit (#696)",
+          "timestamp": "2022-09-14T13:00:12+02:00",
+          "tree_id": "032b667fb9d1fd535cdf1d71ef5cee3f70c3690e",
+          "url": "https://github.com/useblocks/sphinx-needs/commit/48d46667e868140524da763b9bde3fa18f0a08cf"
+        },
+        "date": 1663153504529,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Small, basic Sphinx-Needs project",
+            "value": 0.29554064599994945,
+            "unit": "s",
+            "extra": "Commit: 48d46667e868140524da763b9bde3fa18f0a08cf\nBranch: master\nTime: 2022-09-14T13:00:12+02:00"
+          },
+          {
+            "name": "Official Sphinx-Needs documentation (without services)",
+            "value": 75.89151173300002,
+            "unit": "s",
+            "extra": "Commit: 48d46667e868140524da763b9bde3fa18f0a08cf\nBranch: master\nTime: 2022-09-14T13:00:12+02:00"
           }
         ]
       }
