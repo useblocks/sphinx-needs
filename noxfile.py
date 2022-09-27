713c34bac69fab3aeca886866d2b9afe9412ee46
Daniel Woste <daniel.woste@useblocks.com>
2022-09-27 10:36:17 +0200

    Updating CI for docutils

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,7 @@ from nox_poetry import session
 
 PYTHON_VERSIONS = ["3.8", "3.10"]
 SPHINX_VERSIONS = ["5.2.1", "4.5.0"]
+DOCUTILS_VERSIONS = ["0.19", "0.15"]
 TEST_DEPENDENCIES = [
     "pytest",
     "pytest-xdist",
@@ -18,25 +19,27 @@ TEST_DEPENDENCIES = [
 BENCHMARK_DEPENDENCIES = ["memray"]
 
 
-def is_supported(python: str, sphinx: str) -> bool:
-    return not (python == "3.6" and sphinx not in ["3.2"])
+def is_supported(python: str, sphinx: str, docutils: str) -> bool:
+    return not (sphinx in ["5.2.1"] and docutils in ["0.13"])
 
 
-def run_tests(session, sphinx):
+def run_tests(session, sphinx, docutils):
     session.install(".")
     session.install(*TEST_DEPENDENCIES)
     session.run("pip", "install", "-r", "docs/requirements.txt", silent=True)
     session.run("pip", "install", f"sphinx=={sphinx}", silent=True)
+    session.run("pip", "install", f"docutils=={docutils}", silent=True)
     session.run("echo", "TEST FINAL PACKAGE LIST")
     session.run("pip", "freeze")
     session.run("make", "test", external=True)
 
 
 @session(python=PYTHON_VERSIONS)
-@nox.parametrize("sphinx", SPHINX_VERSIONS)
-def tests(session, sphinx):
-    if is_supported(session.python, sphinx):
-        run_tests(session, sphinx)
+@nox.parametrize("docutils", DOCUTILS_VERSIONS)  # order is important, last options needs to be given first ...
+@nox.parametrize("sphinx", SPHINX_VERSIONS)  # ... by GH workflow
+def tests(session, sphinx, docutils):
+    if is_supported(session.python, sphinx, docutils):
+        run_tests(session, sphinx, docutils)
     else:
         session.skip("unsupported combination")
 
