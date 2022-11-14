46c3e52e3bfac2f33994e2f4c3449ce88f1b6a99
Randy Duodu (He/Him) <duodurandy19@gmail.com>
2022-11-14 09:01:27 +0000

    Obfuscate credentials when using HTTP auth

---
+++ tests/test_clean_log.py
@@ -0,0 +1,24 @@
+import unittest
+
+from sphinx_needs.utils import clean_log
+
+
+class CleanLogTestCase(unittest.TestCase):
+    def test_external_needs_clean_log(self):
+        self.assertEqual(clean_log("http://user:password@host.url/"), "http://****:****@host.url/")
+        self.assertEqual(
+            clean_log("Downloading file from https://daniel:my_password@server.com now"),
+            "Downloading file from https://****:****@server.com now",
+        )
+        self.assertEqual(
+            clean_log(
+                "json_path and json_url are both configured, but only one is allowed. "
+                "json_path: '/v1/needs.json' json_url: 'www.divstack:pswd@gh.com'."
+            ),
+            "json_path and json_url are both configured, but only one is allowed. "
+            "json_path: '/v1/needs.json' json_url: 'www.****:****@gh.com'.",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
