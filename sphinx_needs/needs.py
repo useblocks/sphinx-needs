f00620b4b60bff6c6e6fc332bb90fc4880bb2fe9
Daniel Woste <daniel.woste@useblocks.com>
2022-11-22 13:24:17 +0100

    Version 1.1.0 preparation

--- sphinx_needs/needs.py
+++ sphinx_needs/needs.py
@@ -102,7 +102,7 @@ from sphinx_needs.services.open_needs import OpenNeedsService
 from sphinx_needs.utils import INTERNALS, NEEDS_FUNCTIONS, node_match
 from sphinx_needs.warnings import process_warnings
 
-VERSION = "1.0.3"
+VERSION = "1.1.0"
 NEEDS_FUNCTIONS.clear()
 
 
