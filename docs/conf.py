81e805637b4830892407a196e10109d15a668269
Randy Duodu (He/Him) <duodurandy19@gmail.com>
2022-11-22 09:09:01 +0000

    Added JS test under tests directory

--- docs/conf.py
+++ docs/conf.py
@@ -502,7 +502,6 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
-html_extra_path = ["js_tests"]
 
 # -- Options for HTMLHelp output ------------------------------------------
 
