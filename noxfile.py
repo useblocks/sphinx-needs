7161eb4258ea8e178f169d352610df6ded077f88
Chris Sewell <chrisj_sewell@hotmail.com>
2023-08-03 16:08:37 +0200

    update

--- noxfile.py
+++ noxfile.py
@@ -56,10 +56,6 @@ def tests(session, sphinx, docutils):
 @session(python="3.10")
 def linkcheck(session):
     session.install(".")
-    # LinkCheck can handle rate limits since Sphinx 3.4, which is needed as
-    # our doc has too many links to GitHub.
-    session.run("pip", "install", "sphinx==3.5.4", silent=True)
-
     session.run("pip", "install", "-r", "docs/requirements.txt", silent=True)
     session.run("make", "docs-linkcheck", external=True)
 
