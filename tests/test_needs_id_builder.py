ac7c5fe36e3996eb346e1aeee06c8e28b84b29b2
Namnn07 <nhatnamnguyen.gtvthcm@gmail.com>
2023-08-07 18:52:47 +0700

    add test-case and fix some setting for need per id builder

--- tests/test_needs_id_builder.py
+++ tests/test_needs_id_builder.py
@@ -1,12 +1,17 @@
 import json
 from pathlib import Path
+
 import pytest
 
 
-@pytest.mark.parametrize("test_app", [{"buildername": "needs_id", "srcdir": "doc_test/docs_needs_id_builder"}], indirect=True)
+@pytest.mark.parametrize(
+    "test_app", [{"buildername": "needs_id", "srcdir": "doc_test/docs_needs_id_builder"}], indirect=True
+)
 def test_doc_needs_id_builder(test_app):
     import os
+
     from sphinx_needs.utils import unwrap
+
     app = test_app
     app.build()
     out_dir = app.outdir
@@ -20,10 +25,9 @@ def test_doc_needs_id_builder(test_app):
         need_file_name = f"{need_id}.json"
         needs_json = Path(needs_id_path, need_file_name)
         assert os.path.exists(needs_json)
-  
+
         with open(needs_json) as needs_file:
             needs_file_content = needs_file.read()
 
         needs_list = json.loads(needs_file_content)
         assert needs_list[need_id]["docname"]
-    
