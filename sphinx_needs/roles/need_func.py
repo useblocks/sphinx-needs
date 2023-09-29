ce247a6f1523717ca9a5dd83079d5c3eb262fe3d
Chris Sewell <chrisj_sewell@hotmail.com>
2023-09-29 08:56:30 -0300

    🔧 Add strict typing for `sphinx_needs.functions.functions` (#1042)

--- sphinx_needs/roles/need_func.py
+++ sphinx_needs/roles/need_func.py
@@ -23,6 +23,6 @@ def process_need_func(
     env = app.env
     # for node_need_func in doctree.findall(NeedFunc):
     for node_need_func in found_nodes:
-        result = check_and_get_content(node_need_func.attributes["reftarget"], {"id": "need_func_dummy"}, env)
+        result = check_and_get_content(node_need_func.attributes["reftarget"], {"id": "need_func_dummy"}, env)  # type: ignore
         new_node_func = nodes.Text(str(result))
         node_need_func.replace_self(new_node_func)
