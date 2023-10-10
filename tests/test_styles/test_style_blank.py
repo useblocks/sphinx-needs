d16e73089c0665d70f0cc590e715efc5b7518130
Marco Heinemann <marco.heinemann@useblocks.com>
2023-10-10 21:20:32 +0200

    css_files property needs no reset

--- tests/test_styles/test_style_blank.py
+++ tests/test_styles/test_style_blank.py
@@ -6,11 +6,6 @@ from sphinx.builders.html import StandaloneHTMLBuilder
 
 @pytest.mark.parametrize("test_app", [{"buildername": "html", "srcdir": "doc_test/doc_style_blank"}], indirect=True)
 def test_doc_style_blank(test_app):
-    # css_files is not cleared between test runs so css files get
-    # progressively added.  This forces it to clear before re-building
-    if hasattr(StandaloneHTMLBuilder, "css_files"):
-        del StandaloneHTMLBuilder.css_files[:]
-
     app = test_app
     app.build()
     html = Path(app.outdir, "index.html").read_text()
