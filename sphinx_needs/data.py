f06c4afe7c29e9ac5ed7965001e28faf308f92c6
PhilipPartsch <95444300+PhilipPartsch@users.noreply.github.com>
2024-03-25 09:34:58 +0100

    added comment for content_offset as suggested in review comment
    Co-authored-by: Chris Sewell <chrisj_sewell@hotmail.com>

--- sphinx_needs/data.py
+++ sphinx_needs/data.py
@@ -119,6 +119,9 @@ class NeedsInfoType(TypedDict, total=False):
     post_template: Required[None | str]
     content: Required[str]
     content_offset: Required[int | None]
+    """The offset relative to the ``lineno``,
+    where the content of the directive starts in the source.
+    """
     pre_content: str
     post_content: str
     content_id: Required[None | str]
