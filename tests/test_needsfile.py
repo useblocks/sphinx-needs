7e02e8239fad385964b0a1134348f1b2e54bac1f
Philip Partsch <philip.partsch@bosch.com>
2024-03-26 14:28:32 +0100

    changed test to check if lineno is part of a need in the needs.json

--- tests/test_needsfile.py
+++ tests/test_needsfile.py
@@ -1,5 +1,6 @@
 import pytest
-
+from pathlib import Path
+import json
 
 @pytest.mark.parametrize(
     "test_app",
@@ -9,3 +10,15 @@ import pytest
 def test_doc_build_html(test_app):
     app = test_app
     app.build()
+
+    needs_json_path = Path(app.outdir, "needs.json")
+
+    assert needs_json_path.exists()
+
+    needs_json_content = needs_json_path.read_text()
+    nj = json.loads(needs_json_content)
+
+    for version, vdata in nj['versions'].items():
+        if nj['current_version'] == version:
+            for need_id, need_data in vdata['needs'].items():
+                assert 'lineno' in need_data
