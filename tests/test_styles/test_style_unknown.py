58d1be96bd4006283639be96fe465bc43495e09b
Chris Sewell <chrisj_sewell@hotmail.com>
2024-05-10 17:00:16 +0200

    Update test_style_unknown.py

--- tests/test_styles/test_style_unknown.py
+++ tests/test_styles/test_style_unknown.py
@@ -3,11 +3,18 @@ from pathlib import Path
 import pytest
 from sphinx import version_info
 from sphinx.builders.html import StandaloneHTMLBuilder
+from sphinx.util.console import strip_colors
 
 
 @pytest.mark.parametrize(
     "test_app",
-    [{"buildername": "html", "srcdir": "doc_test/doc_style_unknown"}],
+    [
+        {
+            "buildername": "html",
+            "srcdir": "doc_test/doc_style_unknown",
+            "no_plantuml": True,
+        }
+    ],
     indirect=True,
 )
 def test_doc_style_unknown(test_app):
@@ -22,6 +29,12 @@ def test_doc_style_unknown(test_app):
 
     app = test_app
     app.build()
+
+    warnings = strip_colors(app._warning.getvalue()).splitlines()
+    assert warnings == [
+        "WARNING: needs_css not an existing file: UNKNOWN.css [needs.config]"
+    ]
+
     html = Path(app.outdir, "index.html").read_text()
-    assert "modern_colors.css" not in html
+    assert "modern.css" not in html
     assert "UNKNOWN.css" not in html
