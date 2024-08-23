746c88026ddcca217d3abc0af5f0ad95580e2593
Chris Sewell <chrisj_sewell@hotmail.com>
2024-08-23 06:43:04 +0200

    Update data.py

--- sphinx_needs/data.py
+++ sphinx_needs/data.py
@@ -277,7 +277,7 @@ NeedsCoreFields: Final[Mapping[str, CoreFieldParameters]] = {
     },
     "section_name": {
         "description": "Simply the first section.",
-        "schema": {"type": "string", "default": ""},
+        "schema": {"type": ["string", "null"], "default": ""},
     },
     "signature": {
         "description": "Derived from a docutils desc_name node.",
