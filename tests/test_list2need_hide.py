a25760ec215ddd6f5f06506f75e14bf9d6a94c30
OFFICE\cs <c.seyler@deltatec.be>
2024-12-12 11:12:46 +0100

    add a test description

--- tests/test_list2need_hide.py
+++ tests/test_list2need_hide.py
@@ -14,8 +14,16 @@ from sphinx_needs.api import get_needs_view
     indirect=True,
 )
 def test_doc_list2need_hide(test_app, snapshot):
+    """
+    The test validates the list2need directive with the hide option.
+    The needs must be valid, but the rendered output must not contain the need content.
+    
+    To validate that the needs are valid, the needs are rendered using a needtable directive.
+    """
     app = test_app
     app.build()
+
+
 
     index_html = Path(app.outdir, "index.html").read_text()
     assert '<tr class="need row-even"><td class="needs_id"><p><a class="reference internal" href="#NEED-A">NEED-A</a></p></td>' in index_html
