663d8d4d496cb44a42d4ef801d4ca9ff1426e9bc
Chris Sewell <chrisj_sewell@hotmail.com>
2025-01-27 14:03:31 +0100

    ♻️ Don't process dynamic functions on internal need fields (#1387)
    For most "internal" need fields it does not make sense that these would
    be dynamic, and anyway this would fail since their values are not string
    types.
    
    In this PR, we skip dynamic function processing, for core fields that
    should not be altered by the user.

--- sphinx_needs/data.py
+++ sphinx_needs/data.py
@@ -62,6 +62,8 @@ class CoreFieldParameters(TypedDict):
     """JSON schema for the field."""
     allow_extend: NotRequired[bool]
     """Whether field can be modified by needextend (False if not present)."""
+    allow_df: NotRequired[bool]
+    """Whether dynamic functions are allowed for this field (False if not present)."""
     show_in_layout: NotRequired[bool]
     """Whether to show the field in the rendered layout of the need by default (False if not present)."""
     exclude_external: NotRequired[bool]
@@ -96,21 +98,25 @@ NeedsCoreFields: Final[Mapping[str, CoreFieldParameters]] = {
     "full_title": {
         "description": "Title of the need, of unlimited length.",
         "schema": {"type": "string", "default": ""},
+        "allow_df": True,
     },
     "title": {
         "description": "Title of the need, trimmed to a maximum length.",
         "schema": {"type": "string"},
+        "allow_df": True,
     },
     "status": {
         "description": "Status of the need.",
         "schema": {"type": ["string", "null"], "default": None},
         "show_in_layout": True,
+        "allow_df": True,
         "allow_extend": True,
     },
     "tags": {
         "description": "List of tags.",
         "schema": {"type": "array", "items": {"type": "string"}, "default": []},
         "show_in_layout": True,
+        "allow_df": True,
         "allow_extend": True,
     },
     "collapse": {
@@ -144,6 +150,7 @@ NeedsCoreFields: Final[Mapping[str, CoreFieldParameters]] = {
         "schema": {"type": ["string", "null"], "default": None},
         "show_in_layout": True,
         "exclude_external": True,
+        "allow_df": True,
         "allow_extend": True,
     },
     "arch": {
@@ -174,12 +181,14 @@ NeedsCoreFields: Final[Mapping[str, CoreFieldParameters]] = {
     "type": {
         "description": "Type of the need.",
         "schema": {"type": "string", "default": ""},
+        "allow_df": True,
     },
     "type_name": {
         "description": "Name of the type.",
         "schema": {"type": "string", "default": ""},
         "exclude_external": True,
         "exclude_import": True,
+        "allow_df": True,
     },
     "type_prefix": {
         "description": "Prefix of the type.",
@@ -187,6 +196,7 @@ NeedsCoreFields: Final[Mapping[str, CoreFieldParameters]] = {
         "exclude_json": True,
         "exclude_external": True,
         "exclude_import": True,
+        "allow_df": True,
     },
     "type_color": {
         "description": "Hexadecimal color code of the type.",
@@ -194,6 +204,7 @@ NeedsCoreFields: Final[Mapping[str, CoreFieldParameters]] = {
         "exclude_json": True,
         "exclude_external": True,
         "exclude_import": True,
+        "allow_df": True,
     },
     "type_style": {
         "description": "Style of the type.",
@@ -201,6 +212,7 @@ NeedsCoreFields: Final[Mapping[str, CoreFieldParameters]] = {
         "exclude_json": True,
         "exclude_external": True,
         "exclude_import": True,
+        "allow_df": True,
     },
     "is_modified": {
         "description": "Whether the need was modified by needextend.",
@@ -299,6 +311,7 @@ NeedsCoreFields: Final[Mapping[str, CoreFieldParameters]] = {
     "constraints": {
         "description": "List of constraint names, which are defined for this need.",
         "schema": {"type": "array", "items": {"type": "string"}, "default": []},
+        "allow_df": True,
         "allow_extend": True,
     },
     "constraints_results": {
