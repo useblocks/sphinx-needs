08ea02a012944cf8e182326fd1cb8670feeaa36e
Chris Sewell <chrisj_sewell@hotmail.com>
2025-02-19 00:08:44 +0100

    Update needimport.py

--- sphinx_needs/directives/needimport.py
+++ sphinx_needs/directives/needimport.py
@@ -256,8 +256,9 @@ class NeedimportDirective(SphinxDirective):
             # These keys need to be different for add_need() api call.
             need_params["need_type"] = need_params.pop("type", "")  # type: ignore[misc,typeddict-unknown-key]
             need_params["title"] = need_params.pop(
-                "full_title", need_params.get("title", "")
-            )  # type: ignore[misc]
+                "full_title",  # type: ignore[misc]
+                need_params.get("title", ""),
+            )
 
             # Replace id, to get unique ids
             need_id = need_params["id"] = id_prefix + need_params["id"]
