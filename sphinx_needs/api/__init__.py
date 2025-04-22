13720f9eba1d5679ea2f379c3641d31df96bd349
Marco Heinemann <marco.heinemann@useblocks.com>
2025-04-22 22:38:20 +0200

    Use local import

--- sphinx_needs/api/__init__.py
+++ sphinx_needs/api/__init__.py
@@ -1,10 +1,10 @@
-from ..exceptions import InvalidNeedException
 from .configuration import (
     add_dynamic_function,
     add_extra_option,
     add_need_type,
     get_need_types,
 )
+from .exceptions import InvalidNeedException
 from .need import add_external_need, add_need, del_need, generate_need, get_needs_view
 
 __all__ = (
