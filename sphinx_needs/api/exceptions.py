217473d8afb1ad9ce344d91099a29b7d3a0e16fb
Marco Heinemann <marco.heinemann@useblocks.com>
2025-04-22 22:32:31 +0200

    Fix docs build

---
+++ sphinx_needs/api/exceptions.py
@@ -0,0 +1,17 @@
+from sphinx_needs.exceptions import (
+    InvalidNeedException,
+    NeedsApiConfigException,
+    NeedsApiConfigWarning,
+    NeedsConstraintFailed,
+    NeedsInvalidException,
+    NeedsInvalidFilter,
+)
+
+__all__ = [
+    "InvalidNeedException",
+    "NeedsApiConfigException",
+    "NeedsApiConfigWarning",
+    "NeedsConstraintFailed",
+    "NeedsInvalidException",
+    "NeedsInvalidFilter",
+]
