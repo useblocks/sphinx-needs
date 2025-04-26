3f002fcb534ecf86f3059a12c252b36b0441e911
Chris Sewell <chrisj_sewell@hotmail.com>
2025-04-26 12:50:22 +0200

    cjs fixes

--- sphinx_needs/api/exceptions.py
+++ sphinx_needs/api/exceptions.py
@@ -1,17 +1,15 @@
+import warnings
+
 from sphinx_needs.exceptions import (
-    InvalidNeedException,
-    NeedsApiConfigException,
-    NeedsApiConfigWarning,
-    NeedsConstraintFailed,
-    NeedsInvalidException,
-    NeedsInvalidFilter,
+    InvalidNeedException,  # noqa: F401
+    NeedsApiConfigException,  # noqa: F401
+    NeedsApiConfigWarning,  # noqa: F401
+    NeedsConstraintFailed,  # noqa: F401
+    NeedsInvalidException,  # noqa: F401
+    NeedsInvalidFilter,  # noqa: F401
 )
 
-__all__ = [
-    "InvalidNeedException",
-    "NeedsApiConfigException",
-    "NeedsApiConfigWarning",
-    "NeedsConstraintFailed",
-    "NeedsInvalidException",
-    "NeedsInvalidFilter",
-]
+warnings.warn(
+    "'sphinx_needs.api.exceptions' is deprecated. Use 'sphinx_needs.exception' instead.",
+    stacklevel=2,
+)
