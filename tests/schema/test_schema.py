f0f869a4e80ee2b8d0766d1012920668e868fdb0
Marco Heinemann <marco.heinemann@useblocks.com>
2025-05-05 17:40:53 +0200

    Custom fixture

--- tests/schema/test_schema.py
+++ tests/schema/test_schema.py
@@ -1,19 +1,110 @@
-import re
+from collections.abc import Iterator
 from pathlib import Path
+from textwrap import dedent
+from typing import Callable
 
 import pytest
 import sphinx
 from sphinx.testing.util import SphinxTestApp
+from sphinx.util.console import strip_colors
 
 CURR_DIR = Path(__file__).parent
 
 
-def rm_ansi_escapes(text: str) -> str:
-    """
-    Remove ANSI escape sequences from a string.
-    """
-    ansi_escape = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
-    return ansi_escape.sub("", text)
+def get_warnings_list(app: SphinxTestApp):
+    return strip_colors(app._warning.getvalue()).splitlines()
+
+
+UBPROJECT_BASE = """
+[needs]
+schemas_from_json = "schemas.json"
+
+[[needs.extra_options]]
+name = "asil"
+description = "Automotive Safety Integrity Level"
+
+[[needs.types]]
+directive = "feat"
+title = "Feat"
+prefix = "Feat_"
+
+[[needs.types]]
+directive = "req"
+title = "Requirement"
+prefix = "REQ_"
+
+[[needs.types]]
+directive = "spec"
+title = "Specification"
+prefix = "SPEC_"
+"""
+
+schemas_json = """
+[
+  {
+    "severity": "warning",
+    "message": "id must be uppercase",
+    "local_schema": {
+      "properties": {
+        "id": { "pattern": "^[A-Z0-9_]+$" }
+      }
+    }
+  }
+]
+"""
+
+CONF_PY_BASE = """
+extensions = ["sphinx_needs"]
+needs_from_toml = "ubproject.toml"
+"""
+
+
+@pytest.mark.parametrize(
+    "ubproject_schemas_rst",
+    [
+        [
+            UBPROJECT_BASE
+            + dedent("""
+                [[needs.extra_options]]
+                name = "efforts"
+                [needs.extra_options.schema]
+                type = "integer" 
+                """),
+            dedent("""
+                []
+                """),
+            dedent("""
+                .. feat:: feat wrong id
+                   :id: FEAT_01
+                   :asil: QM
+                """),
+        ]
+    ],
+)
+def test_schema_option_type(
+    tmpdir: Path,
+    ubproject_schemas_rst: tuple[str, str, str],
+    make_app: Iterator[Callable[[], SphinxTestApp]],
+):
+    """Test matching option type."""
+    conf_py_path = tmpdir / "conf.py"
+    conf_py_path.write_text(CONF_PY_BASE, encoding="utf-8")
+
+    ubproject_content, schemas_content, rst_content = ubproject_schemas_rst
+    toml_path = tmpdir / "ubproject.toml"
+    json_path = tmpdir / "schemas.json"
+    rst_path = tmpdir / "index.rst"
+
+    toml_path.write_text(ubproject_content, encoding="utf-8")
+    json_path.write_text(schemas_content, encoding="utf-8")
+    rst_path.write_text(rst_content, encoding="utf-8")
+
+    app: SphinxTestApp = make_app(srcdir=Path(tmpdir), freshenv=True)
+
+    app.build()
+    assert app.statuscode == 0
+    warnings = get_warnings_list(app)
+    assert len(warnings) == 0
 
 
 @pytest.mark.parametrize(
@@ -24,7 +115,7 @@ def rm_ansi_escapes(text: str) -> str:
 def test_schema_basic(test_app: SphinxTestApp) -> None:
     test_app.builder.build_all()
 
-    warnings_raw = rm_ansi_escapes(test_app.warning.getvalue())
+    warnings_raw = strip_colors(test_app.warning.getvalue())
     warnings = [part for part in warnings_raw.split("WARNING: ") if part]
 
     expected_warnings = [
