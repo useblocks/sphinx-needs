e4485fa3f9d7fd35df4806464d13055c2b8b1650
PhilipPartsch <95444300+PhilipPartsch@users.noreply.github.com>
2025-05-07 23:13:15 +0200

    in calc_sum added needs.values() as parameter to filter_single_need()

--- sphinx_needs/functions/common.py
+++ sphinx_needs/functions/common.py
@@ -383,7 +383,7 @@ def calc_sum(
     for check_need in check_needs:
         if filter:
             try:
-                if not filter_single_need(check_need, needs_config, filter):
+                if not filter_single_need(check_need, needs_config, filter, needs = needs.values()):
                     continue
             except ValueError:
                 pass
