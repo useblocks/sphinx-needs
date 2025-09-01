2c5de928a4d2ba3afe1e1c6962f12b58b68922e0
PhilipPartsch <95444300+PhilipPartsch@users.noreply.github.com>
2025-09-01 23:08:50 +0200

    return warning node and omit warning

--- sphinx_needs/directives/needimport.py
+++ sphinx_needs/directives/needimport.py
@@ -92,12 +92,18 @@ class NeedimportDirective(SphinxDirective):
             )[1]
 
             if not os.path.exists(correct_need_import_path):
+                warning_text = f"Could not load needs import file {correct_need_import_path}"
                 log_warning(
                     logger,
-                    f"Could not load needs import file {correct_need_import_path}",
+                    warning_text,
                     "needimport",
                     location=(self.env.docname, self.lineno),
                 )
+
+                paragraph = nodes.paragraph(text=warning_text)
+                warning = nodes.warning()
+                warning += paragraph
+                return [warning]
 
             try:
                 with open(correct_need_import_path) as needs_file:
