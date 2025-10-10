461256b20e37350a99fb85810cf21483c7aba6a1
Chris Sewell <chrisj_sewell@hotmail.com>
2025-10-10 17:58:33 +0200

    fix test

--- tests/doc_test/parallel_doc/conf.py
+++ tests/doc_test/parallel_doc/conf.py
@@ -31,6 +31,13 @@ needs_types = [
     },
 ]
 needs_variants = {"change_author": "assignee == 'Randy Duodu'"}
-needs_variant_options = ["status", "author"]
 needs_filter_data = {"assignee": "Randy Duodu"}
-needs_extra_options = ["my_extra_option", "another_option", "author", "comment"]
+needs_core_options = {
+    "status": {"variant_functions": True},
+}
+needs_extra_options = [
+    "my_extra_option",
+    "another_option",
+    {"name": "author", "variant_functions": True},
+    "comment",
+]
