94f378843d4e07822e1752a3e2cd41b08822c2f6
Marco Heinemann <marco.heinemann@useblocks.com>
2025-10-22 21:40:59 +0200

    Fix Sphinx 7.4

--- sphinx_needs/logging.py
+++ sphinx_needs/logging.py
@@ -126,3 +126,32 @@ def log_warning(
         color=color,
         once=once,
     )
+
+
+def log_error(
+    logger: SphinxLoggerAdapter,
+    message: str,
+    subtype: WarningSubTypes,
+    /,
+    location: str | tuple[str | None, int | None] | Node | None,
+    *,
+    color: str | None = None,
+    once: bool = False,
+    type: str = "needs",
+) -> None:
+    # Since sphinx in v7.3, sphinx will show warning types if `show_warning_types=True` is set,
+    # and in v8.0 this was made the default.
+    if version_info < (8,):
+        if subtype:
+            message += f" [{type}.{subtype}]"
+        else:
+            message += f" [{type}]"
+
+    logger.error(
+        message,
+        type=type,
+        subtype=subtype,
+        location=location,
+        color=color,
+        once=once,
+    )
